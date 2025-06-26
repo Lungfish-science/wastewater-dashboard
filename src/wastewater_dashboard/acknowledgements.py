b2f30a6bc7dfa2ea0e663d7c791c0a64a75e3e58
William-Gardner-Biotech <will.k.gardner@gmail.com>
2025-06-26 11:54:01 -0500

    weekly update plus acknowledgements
    Changed the GT plotting function so I can pass the last updated date
    through another file and make 1 less file update each time I acknowledge
    updates.

--- src/wastewater_dashboard/acknowledgements.py
+++ src/wastewater_dashboard/acknowledgements.py
@@ -8,7 +8,7 @@ from great_tables import GT, nanoplot_options
 from loguru import logger
 
 
-def convert_table_to_greatable(df_path: Path) -> GT:
+def convert_table_to_greatable(df_path: Path, last_updated: str) -> GT:
     """
     Converts a Polars DataFrame to an aggregated form with metadata per study_accession.
 
@@ -147,7 +147,7 @@ def convert_table_to_greatable(df_path: Path) -> GT:
     # This step comes last
     submitters = (
         GT(aggregated_df, rowname_col="Study Title")
-        .tab_header(title="Sequence Read Archive SARS-CoV-2 Wastewater Submissions", subtitle=f"Last Updated: 2025-06-01")
+        .tab_header(title="Sequence Read Archive SARS-CoV-2 Wastewater Submissions", subtitle=last_updated)
         .tab_spanner(label="Dates", columns=[
             "Earliest Submission",
             "Most Recent Submission"])
